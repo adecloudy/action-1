5d8bc8162cb296f83bcce2ecd2c2467b44916f8f
adecloudy <adecloudy@gmail.com>
2025-06-24 17:10:43 -0400

    upgraded code base

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,5 +1,5 @@
-#/bin/bash
+#!/bin/bash
 sudo apt-get install cowsay -y
-cowsay -f dragon "Run for cover, I am a Dragon ... RAWR" >>dragon.txt
-grep -i "dragon.txt"
+cowsay -f dragon "Run for cover, I am a Dragon ... RAWR" > dragon.txt
+grep -i "dragon" dragon.txt
 ls -ltra
