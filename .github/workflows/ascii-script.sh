8ad0a8f314fef33de1dd65c52aec545231d1e996
adecloudy <adecloudy@gmail.com>
2025-06-26 13:22:39 -0400

    reupload

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -5,7 +5,7 @@ sudo apt-get update
 sudo apt-get install cowsay -y
 
 # Generate ASCII art and write to file
-cowsay -f dragon "Run for cover, I am a Dragon ... RAWR" > dragon.txt
+cowsay -f dragon "Run for cover, I am a Dragon ... RAWR" >> dragon.txt
 
 # Search for the word "dragon" in the file
 grep -i "dragon" dragon.txt
